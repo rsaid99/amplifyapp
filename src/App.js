1ea5e9d978df4a826157506f8133e1ade96c98d7
Raoul <rsaid.2013@gmail.com>
2020-12-02 15:58:07 -0800

    v4 test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-	<h1> V3 is all the rage! :HELLO FROM V2 </h1>
+	<h1> V4 </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
